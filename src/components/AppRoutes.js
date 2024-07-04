62c738982aa6f64cda5d29c7c1dd0df8dac2ff49
Your Name <your.email@example.com>
2024-07-04 14:22:01 +0530

    index.html file updated

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -18,7 +18,7 @@ import ImageGallery from '../pages/ImageGallary';
 
 const AppRoutes = () => {
   return (
-    <Router basename='/'>
+    <Router>
       <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
